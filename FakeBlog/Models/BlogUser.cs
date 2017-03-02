052cd0e4f989aed068f6944664e0121c8cc28598
ostrander-nathan <ostrander_nathan@yahoo.com>
2017-03-01 19:15:15 -0600

    blog complete

---
+++ FakeBlog/Models/BlogUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class BlogUser
+    {
+        [Key]
+        public int BlogUserID { get; set; } // Primary Key
+
+        [MinLength(10)]
+        [MaxLength(60)]
+        public string Email { get; set; }
+
+        [MaxLength(60)]
+        public string Username { get; set; }
+
+        [MaxLength(60)]
+        public string Fullname { get; set; }
+
+        public ApplicationUser BaseUser { get; set; } // 1 to 1 relationship
+
+        public List<Post> Posts { get; set; } // 1 to many (Posts) relationship
+
+
+    }
+}
---
+++ FakeBlog/Models/Draft.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Draft
+    {
+        [Key]
+        public int DraftId { get; set; }
+
+        public List<Published> ArePublished { get; set; }
+
+    }
+}
